eeb41134cc2b026776eb36379cae7b17c63d773a
Khenan <knewton@plotarmorr.com>
2024-09-23 06:55:29 -0400

    (After 2 hour limit) Update Comments for SQLQuery_Contacts

--- Database Query/SQLQuery_Contacts.sql
+++ Database Query/SQLQuery_Contacts.sql
@@ -1,7 +1,8 @@
+--Database creation
 CREATE DATABASE MailingList;
-
 USE MailingList;
 
+--Table creation
 CREATE TABLE Contact
 (
 Contact_ID int identity(1,1) Primary Key,
@@ -10,8 +11,7 @@ LastName varchar(50),
 Email varchar(50)
 );
 
-select * from contact;
-
+--Starter Data. Use these queries to have some initial data to test the application.
 INSERT INTO Contact values ('Frederick', 'Hamlin', 'fhamlin@example.com');
 INSERT INTO Contact values ('Derik', 'Hamlin', 'dhamlin@example.com');
 INSERT INTO Contact values ('Jaya', 'Newton', 'jnewton@yahoo.com');
